124e2f0726c916233f587328da6a21ca0d892ac7
phota16 <prabasini.hota@gmail.com>
2018-05-08 21:45:48 -0500

    updated unit test cases

--- ColorCodeCalculator.Tests/Controllers/HomeControllerTest.cs
+++ ColorCodeCalculator.Tests/Controllers/HomeControllerTest.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ColorCodeCalculator;
 using ColorCodeCalculator.Controllers;
+using ColorCodeCalculator.BLogic;
 
 namespace ColorCodeCalculator.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
+        int colBands1 = 2;
+        int colBands2 = 3;
+        float colBands3 = 10;
+        float colBands4 = 2;
+        float expectedResult = 230f;
+      
         [TestMethod]
         public void Index()
         {
@@ -24,6 +26,19 @@ namespace ColorCodeCalculator.Tests.Controllers
             // Assert
             Assert.IsNotNull(result);
         }
+        [TestMethod]
+        public void IndexPost()
+        {
+            // Arrange           
+            ResistorOhmCalculator obj = new BLogic.ResistorOhmCalculator();
+
+            //Act
+            float result = obj.CalculateOhmValue(colBands1, colBands2, colBands3, colBands4);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+
+        }       
 
         [TestMethod]
         public void About()
